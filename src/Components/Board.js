8b9a9ec9d23d429bd9cd881023244f2c4aad6164
ILRZV <ilyarazuvaev13@gmail.com>
2021-07-30 18:00:14 +0300

    'no_it_works'

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,46 +3,31 @@ import "../css/style.css";
 import Square from "./Square";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       ></Square>
     );
   }
-  handleClick(i) {
-    let square = this.state.squares.slice();
-    square[i] = "X";
-
-    this.setState({ squares: square });
-  }
   render() {
     return (
-      <div>
-        <span className="next_player_anonce">Next Player</span>
-        <div className="board">
-          <div className="board_row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board_row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board_row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+      <div className="board">
+        <div className="board_row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board_row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board_row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,19 +1,95 @@
 import React from "react";
 import "../css/style.css";
 import Board from "./Board";
-import Square from "./Square";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (caclulateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = caclulateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = "Победитель: " + winner;
+    } else {
+      status = "Следующий игрок: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
         <div className="game_board">
-          <Board></Board>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          ></Board>
         </div>
-        <div className="game_info"></div>
+        <div className="game_info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
       </div>
     );
   }
+}
+
+function caclulateWinner(squares) {
+  let lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    let [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Game;
